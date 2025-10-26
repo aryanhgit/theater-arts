d1c5fd14f08771df05d9160369cb5dfbf9613f5f
ardawgcl3 <150267246+aryanhgit@users.noreply.github.com>
2025-10-26 15:09:46 +0530

    Deploy Issue Fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
   base: process.env.VITE_BASE_PATH || "/theater-arts",
 })
